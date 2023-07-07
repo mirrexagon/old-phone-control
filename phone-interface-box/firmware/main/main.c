e8cafbc8fe85063d7869a6e7c0a213aa7590b4af
Andrew Abbott <andrew@mirx.dev>
2023-07-07 18:57:56 +1000

    Add pin definitions for breadboard prototype

--- phone-interface-box/firmware/main/main.c
+++ phone-interface-box/firmware/main/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+#include "gpio.h"
+
+const gpio_num_t GPIO_OFF_HOOK = 4;
+const gpio_num_t GPIO_RING_MODE = 5;
+const gpio_num_t GPIO_RING_WAVE = 6;
+const gpio_num_t GPIO_AUDIO_OUT = 17; // DAC_1
+const gpio_num_t GPIO_AUDIO_IN = 1; // ADC1_CH0
+
 void app_main(void)
 {
 
