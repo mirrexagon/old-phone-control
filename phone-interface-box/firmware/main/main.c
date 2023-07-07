a3e028c0b2b047deefda59659d0ad52a1f94908e
Andrew Abbott <andrew@mirx.dev>
2023-07-07 20:27:19 +1000

    Add test ringing code
    It rings!

--- phone-interface-box/firmware/main/main.c
+++ phone-interface-box/firmware/main/main.c
@@ -6,6 +6,8 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
 
 const uint32_t GPION_OFF_HOOK = 4; // High for off-hook, low for on-hook. SHK pin on KS0835.
 const uint32_t GPION_RING_MODE = 5; // RM pin on KS0835.
@@ -35,7 +37,9 @@ static void gpio_task_example(void* arg)
 
 void app_main(void)
 {
+    // SHK pin and ISR setup.
     gpio_config_t io_conf = {};
+
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pin_bit_mask = 1 << GPION_OFF_HOOK;
     io_conf.intr_type = GPIO_INTR_ANYEDGE;
@@ -47,9 +51,47 @@ void app_main(void)
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
     gpio_isr_handler_add(GPION_OFF_HOOK, off_hook_edge_isr, (void*)GPION_OFF_HOOK);
 
+    // Ringing setup.
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = (1 << GPION_RING_MODE) | (1 << GPION_RING_WAVE);
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    gpio_config(&io_conf);
+
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_LOW_SPEED_MODE,
+        .timer_num        = LEDC_TIMER_0,
+        .duty_resolution  = LEDC_TIMER_13_BIT,
+        .freq_hz          = 25,  // Hz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = LEDC_LOW_SPEED_MODE,
+        .channel        = LEDC_CHANNEL_0,
+        .timer_sel      = LEDC_TIMER_0,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = GPION_RING_WAVE,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
     printf("Minimum free heap size: %"PRIu32" bytes\n", esp_get_minimum_free_heap_size());
 
     while(1) {
+        printf("Ring stop\n");
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
+        ESP_ERROR_CHECK(gpio_set_level(GPION_RING_MODE, 0));
         vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        printf("Ring start\n");
+        ESP_ERROR_CHECK(gpio_set_level(GPION_RING_MODE, 1));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 4095)); // 50% at 13-bit resolution.
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
